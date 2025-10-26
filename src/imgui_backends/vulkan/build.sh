6e75c95a0e2df93081443f0aa6935d253d33952c
Stowy <stowy@protonmail.ch>
2025-10-26 15:32:09 +0100

    In the process of putting the imgui backends in the jai-imgui repo

--- src/imgui_backends/vulkan/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## -V: create SPIR-V binary
-## -x: save binary output as text-based 32-bit hexadecimal numbers
-## -o: output file
-glslc glsl_shader.frag -o glsl_shader.frag.spv
-glslc glsl_shader.vert -o glsl_shader.vert.spv
